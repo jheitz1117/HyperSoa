aea189d084248c11d944fa6451281ef52d4262d0
Josh Heitz <jheitz@wattsandassociates.com>
2023-08-24 09:12:37 -0500

    Add DI extension method to register services for hypernode hosting.

---
+++ HyperSoa.ServiceHosting/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using HyperSoa.Contracts;
+using HyperSoa.Service;
+using HyperSoa.Service.Configuration;
+using HyperSoa.ServiceHosting;
+using HyperSoa.ServiceHosting.Configuration;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class DependencyInjectionExtensions
+    {
+        public static void AddHyperNodeServiceHosting<TServiceConfig, THostConfig>(this IServiceCollection services)
+            where TServiceConfig : class, IHyperNodeConfigurationProvider
+            where THostConfig : class, IHyperNodeHostConfigurationProvider
+        {
+            services.AddTransient<IHyperNodeConfigurationProvider, TServiceConfig>();
+            services.AddSingleton<IHyperNodeService, HyperNodeService>();
+            services.AddHostedService<HostedHyperNodeService>();
+
+            services.AddTransient<IHyperNodeHostConfigurationProvider, THostConfig>();
+            services.AddSingleton<IHyperNodeServiceHost, HyperNodeServiceHost>();
+            services.AddHostedService<HostedListenerService>();
+        }
+    }
+}
